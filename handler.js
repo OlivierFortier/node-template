0b79dff439697c9bed7630a14f9b2be933667d67
Olivier Fortier <olivier_fortier@outlook.com>
2022-02-25 22:51:21 +0000

    atest

--- handler.js
+++ handler.js
@@ -1,5 +1,13 @@
+// const { SubscriptionClient } = require("@azure/arm-resources-subscriptions");
+const { DefaultAzureCredential } = require("@azure/identity");
 
 exports.handler = async (context, inputs) => {
     // console.log('Inputs were ' + JSON.stringify(inputs));
     console.log("salut")
+
+    // const subscriptionId = "f5c25c0b-39d1-48b7-a644-c1f6b4d83b49";
+    // const client = new SubscriptionClient(new DefaultAzureCredential(), subscriptionId);
+    const creds = new DefaultAzureCredential();
+    console.log(JSON.stringify(creds));
+    console.log(creds.getToken());
 }
